6c238340ea5fad66ee3e5d912248ce959eef20ce
JannsenYang <ouyangjingzhou@gmail.com>
2022-04-11 08:33:27 +0800

    祝大家都能吃饱

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -22,7 +22,7 @@ public class Application {
             return;
         }
 //   此为单次执行模式  用于在非高峰期测试下单  也必须满足3个前提条件  1.有收货地址  2.购物车有商品 3.能选择配送信息
-//        Map<String, Object> cartMap = Api.allCheck();
+//        Api.allCheck();
 //        Map<String, Object> cartMap = Api.getCart();
 //        Map<String, Object> multiReserveTimeMap = Api.getMultiReserveTime(UserConfig.addressId, cartMap);
 //        Map<String, Object> checkOrderMap = Api.getCheckOrder(UserConfig.addressId, cartMap, multiReserveTimeMap);
